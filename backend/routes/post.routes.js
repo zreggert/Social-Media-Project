9547751393c7080ea609dec5ed2d0ab4955683fb
Zack Eggert <zreggert@gmail.com>
2024-06-12 21:44:23 -0400

    completed Post routes and controllers

---
+++ backend/routes/post.routes.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import { protectRoute } from '../middleware/protectRoute.js';
+import { 
+    createPost, 
+    deletePost, 
+    commentOnPost, 
+    likeUnlike,
+    getAllPosts,
+    getFollowingPosts,
+    getLikedPosts,
+    getUserPosts
+     } from '../controllers/post.controller.js';
+
+const router = express.Router();
+
+router.get('/all', protectRoute, getAllPosts);
+router.get('/following', protectRoute, getFollowingPosts);
+router.get('/likes/:id', protectRoute, getLikedPosts);
+router.get('/user/:username', protectRoute, getUserPosts);
+router.post('/create', protectRoute, createPost);
+router.post('/like/:id', protectRoute, likeUnlike);
+router.post('/comment/:id', protectRoute, commentOnPost);
+router.delete('/:id', protectRoute, deletePost);
+
+
+export default router;
